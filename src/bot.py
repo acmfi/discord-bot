d314c1c4dadf4be9bf11c9b6dcdeddb71daa0134
botadeoro <43742494+botadeoro@users.noreply.github.com>
2020-08-16 19:58:09 +0200

    He cambiado el texto para que siga algunos patrones de diseño de PEP8

--- src/bot.py
+++ src/bot.py
@@ -14,32 +14,32 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('$Hello'):
         await message.channel.send('Hello!')
-    
+
     if message.content.startswith('$BajarMano'):
-        #Cualquier persona puede silenciarse
-        await message.author.edit(reason= None, mute=True)
-        await message.channel.send('A '+message.author.display_name+' le han bajado la Mano')
-        
+        # Cualquier persona puede silenciarse
+        await message.author.edit(reason=None, mute=True)
+        await message.channel.send('A ' + message.author.display_name + ' le han bajado la Mano')
+
     if message.content.startswith('$LevantarMano'):
-        await message.channel.send(message.author.display_name +' ha levantado la Mano')
+        await message.channel.send(message.author.display_name + ' ha levantado la Mano')
 
     if message.content.startswith('$Mano'):
-        #Falta comprobar que al author de este mensaje sea Ponente 
-        await Mano(message)
-        await message.channel.send('A '+message.author.display_name +' le han levantado la Mano')
+        # Falta comprobar que al author de este mensaje sea Ponente
+        await mano(message)
+        await message.channel.send('A ' + message.author.display_name + ' le han levantado la Mano')
 
-#Aux
-async def Mano(message: discord.Message):
+
+# Aux
+async def mano(message: discord.Message):
     user = message.content.split()
-    channel_members= message.channel.members
+    channel_members = message.channel.members
     for member in channel_members:
         if member.display_name == user[1]:
-            user= member
+            user = member
             break
-    await user.edit(reason= None, mute=False)
-
+    await user.edit(reason=None, mute=False)
 
 
 client.run(TOKEN)
