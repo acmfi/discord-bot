a11bd8c4ab5d3226626b5167a05e4bda589510ea
Carlos González-Almansa Laredo <44754483+Formil@users.noreply.github.com>
2020-08-17 08:31:35 +0200

    Implementación
    Cambios en bot.py para que cumpla la función de mostrar/esconder el canal de texto correspondiente a cada canal de voz, en este caso, suponiendo que el canal asignado a cada canal de voz es uno cuyo nombre es igual, pero acabado en "-texto", en este caso no siendo sensible a las mayúsculas o no. Sujeto a cambios obviamente., pero un acercamiento.

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
 import discord
 
 client = discord.Client()
-TOKEN = open('src/token.txt', 'r').read()
+TOKEN = open('token.txt', 'r').read()
 
 
 @client.event
@@ -10,12 +10,17 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+async def on_voice_state_update(member,before,after):
+    #Si se trata de una acción dentro del mismo canal significa que no ha entrado ni salido de un canal
+    if(before.channel == after.channel):  return 0
+    for channel in member.guild.channels:
+        if str(channel).casefold() in list(map(lambda x : (str(x)+"-texto").casefold(),[after.channel,before.channel])) and channel.type.name=='text':
+            if(before.channel == None):
+                await channel.set_permissions(member,read_messages=True,send_messages= True)
+            else:
+                await channel.set_permissions(member,read_messages= False, send_messages = False)
+            break
+        
 
 
 client.run(TOKEN)
