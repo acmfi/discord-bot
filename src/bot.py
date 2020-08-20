c4e8feb51aea579fb715ad7d5e6df1d005ffcaae
Carlos González-Almansa Laredo <44754483+Formil@users.noreply.github.com>
2020-08-20 21:38:31 +0200

    Update bot.py
    Arreglar mi ineptitud para usar github

--- src/bot.py
+++ src/bot.py
@@ -1,31 +1,3 @@
-<<<<<<< Updated upstream
-import discord
-
-client = discord.Client()
-TOKEN = open('token.txt', 'r').read()
-
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_voice_state_update(member,before,after):
-    #Si se trata de una acción dentro del mismo canal significa que no ha entrado ni salido de un canal
-    if(before.channel == after.channel):  return 0
-    for channel in member.guild.channels:
-        if str(channel).casefold() in list(map(lambda x : (str(x)+"-texto").casefold(),[after.channel,before.channel])) and channel.type.name=='text':
-            if(before.channel == None):
-                await channel.set_permissions(member,read_messages=True,send_messages= True)
-            else:
-                await channel.set_permissions(member,read_messages= False, send_messages = False)
-            break
-        
-
-
-client.run(TOKEN)
-=======
 import discord
 from discord.ext import commands
 
@@ -44,4 +16,3 @@ async def on_ready():
 
 
 bot.run(TOKEN)
->>>>>>> Stashed changes
