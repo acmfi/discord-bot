56e777d1c5586ea6f03284305529952c4b09b5ec
onmax <maximogarciamtnez@gmail.com>
2020-08-22 01:18:35 +0200

    bots reacts to own poll to give users an option

--- src/extensions/poll.py
+++ src/extensions/poll.py
@@ -1,15 +1,47 @@
 import re
 from discord.ext import commands
-
+from discord.utils import get
 
 def setup(bot):
     bot.add_cog(Poll(bot))
 
 
 class PollModel:
-    def __init__(self, question, options):
+    emojis = {
+        "short": [":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:",
+                  ":keycap_ten:"],
+        "unicode": ["{}\N{COMBINING ENCLOSING KEYCAP}".format(num) for num in range(1, 10)]
+    }
+
+    def __init__(self, question=None, options=None):
+        if question is None or options is None:
+            return
         self.question = question
         self.options = options
+
+    def from_message(self, message):
+        self.message = message
+        self.question, self.options = self.poll_parser(self.message.clean_content)
+        self.set_poll_str()
+        return self
+
+    def poll_parser(self, msg):
+        regex = r"\"(.+?)\""
+        matches = list(re.finditer(regex, msg, re.MULTILINE))
+        if len(matches) < 3 or len(matches) > 11 or matches[-1].end() != len(msg):
+            raise Exception('Bro, lo has puesto mal. Si quieres un ejemplo mira cruck.\n\n\t'
+                            '/poll "Aquí tu pregunta" "Una opción" "Como mínimo 2 opciones" '
+                            '"Hasta 10 opciones, que guay!"')
+        return matches[0].group(1), [m.group(1) for m in matches[1:]]
+
+    def set_poll_str(self):
+        question = f"**{self.question}**"
+        options = "\n".join([f"{self.emojis['short'][i]}{' '*3}{o}" for i, o in enumerate(self.options)])
+        self.reactions = {
+            "short": self.emojis["short"][:len(self.options)],
+            "unicode": self.emojis["unicode"][:len(self.options)]
+        }
+        self.poll_str = f"{question}\n\n{options}"
 
     def __str__(self):
         options_str = " ".join(['"{}"'.format(o) for o in self.options])
@@ -20,27 +52,22 @@ class PollModel:
             return self.question == other.question and self.options == other.options
 
 
+
 class Poll(commands.Cog):
     def __init__(self, bot_):
         self.bot = bot_
-
-    def poll_parser(self, msg):
-        regex = r"\"(.+?)\""
-        matches = list(re.finditer(regex, msg, re.MULTILINE))
-        if len(matches) < 3 or matches[-1].end() != len(msg):
-            raise Exception('Bro, lo has puesto mal. Si quieres un ejemplo mira cruck.\n\n\t'
-                            '/poll "Aquí tu pregunta" "Una opción" "Como mínimo 2 opciones" '
-                            '"Hasta 9 opciones, que guay!"')
-        return PollModel(matches[0].group(1), [m.group(1) for m in matches[1:]])
 
     @commands.command(name='poll', aliases=['encuesta'], brief='Creates a new poll',
                       description='Creates a poll and the participants will vote one or more options',
                       usage='"Question" "Option 1" ... "Option N"')
     async def create_poll(self, ctx):
         try:
-            poll_model = self.poll_parser(ctx.message.clean_content)
+            poll_model = PollModel().from_message(ctx.message)
+            print(f"Log: New poll. Question: {poll_model.question}. Options: {poll_model.options}")
         except Exception as e:
             await ctx.message.channel.send(f"```{e}```")
+            print(f"Log: Invalid input {ctx.message.clean_content} || {e}")
+            return
 
-        print("A new poll\n\tquestion: {}\n\toptions: {}".format(poll_model.question, poll_model.options))
-        await ctx.message.channel.send("A new poll\n\tquestion: {}\n\toptions: {}".format(poll_model.question, poll_model.options))
+        poll_sent = await ctx.message.channel.send(poll_model.poll_str)
+        [await poll_sent.add_reaction(reaction) for reaction in poll_model.reactions["unicode"]]
--- test/test_poll.py
+++ test/test_poll.py
@@ -2,6 +2,11 @@ import pytest
 
 from src.extensions.poll import PollModel, Poll
 
+class SimulatedMessage:
+    # We are using a Message model given by the Discord API. To simulate that
+    # structure we need to convert the msg to a class like this
+    def __init__(self, msg):
+        self.clean_content = msg
 
 def test_parser1():
     # Structure that the function has to create
@@ -11,7 +16,7 @@ def test_parser1():
     # It contains the following string: '/poll "This is a question" "Option 1" "Option 2"'
     poll_str = str(expected_poll)
 
-    assert Poll(None).poll_parser(poll_str) == expected_poll
+    assert PollModel().from_message(SimulatedMessage(poll_str)) == expected_poll
 
 def test_parser2():
     # Structure that the function has to create
@@ -19,24 +24,72 @@ def test_parser2():
 
     poll_str = '/poll Random text "This is a question" Random text "Option 1" Random text "Option 2"'
 
-    assert Poll(None).poll_parser(poll_str) == expected_poll
+    assert PollModel().from_message(SimulatedMessage(poll_str)) == expected_poll
+
+def test_parser3():
+    # Structure that the function has to create
+    expected_poll = PollModel("q", ["o1", "o2", "o3", "o4", "o5", "o6", "o7", "o8", "o9", "o10"])
+
+    poll_str = '/poll "q" "o1" "o2" "o3" "o4" "o5" "o6" "o7" "o8" "o9" "o10"'
+
+    assert PollModel().from_message(SimulatedMessage(poll_str)) == expected_poll
 
 def test_invalid_input1():
     invalid_command = '/poll "Here your question" "Here an option" "A bad option'
     with pytest.raises(Exception):
-        Poll(None).poll_parser(invalid_command)
+        PollModel().from_message(SimulatedMessage(invalid_command))
 
 def test_invalid_input2():
     invalid_command = '/poll "Here your question" "Only one option"'
     with pytest.raises(Exception):
-        Poll(None).poll_parser(invalid_command)
+        PollModel().from_message(SimulatedMessage(invalid_command))
 
 def test_invalid_input3():
     invalid_command = '/poll "Just a string"'
     with pytest.raises(Exception):
-        Poll(None).poll_parser(invalid_command)
+        PollModel().from_message(SimulatedMessage(invalid_command))
 
 def test_invalid_input4():
     invalid_command = '/poll'
     with pytest.raises(Exception):
-        Poll(None).poll_parser(invalid_command)
+        PollModel().from_message(SimulatedMessage(invalid_command))
+
+def test_invalid_input5():
+    invalid_command = '/poll'
+    with pytest.raises(Exception):
+        PollModel().from_message(SimulatedMessage(invalid_command))
+
+def test_invalid_input6():
+    invalid_command = '/poll "q" "o1" "o2" "o3" "o4" "o5" "o6" "o7" "o8" "o9" "o10" "o11"'
+    with pytest.raises(Exception):
+        PollModel().from_message(SimulatedMessage(invalid_command))
+
+def test_create_poll_test1():
+    poll_model = PollModel("Here your question", ['Here an option', 'Maybe another option', 'and final option'])
+    expected = "**Here your question**\n\n" \
+               ":one:   Here an option\n" \
+               ":two:   Maybe another option\n" \
+               ":three:   and final option"
+    poll_str = str(poll_model)
+    poll_model_generated = PollModel().from_message(SimulatedMessage(poll_str))
+    assert poll_model_generated.poll_str == expected and \
+           poll_model_generated.reactions["short"] == [":one:", ":two:", ":three:"]
+
+def test_create_poll_test2():
+    poll_model = PollModel("q", ["o1", "o2", "o3", "o4", "o5", "o6", "o7", "o8", "o9", "o10"])
+    expected = "**q**\n\n" \
+               ":one:   o1\n" \
+               ":two:   o2\n" \
+               ":three:   o3\n" \
+               ":four:   o4\n" \
+               ":five:   o5\n" \
+               ":six:   o6\n" \
+               ":seven:   o7\n" \
+               ":eight:   o8\n" \
+               ":nine:   o9\n" \
+               ":keycap_ten:   o10"
+    poll_str = str(poll_model)
+    poll_model_generated = PollModel().from_message(SimulatedMessage(poll_str))
+    assert poll_model_generated.poll_str == expected and \
+           poll_model_generated.reactions["short"] == [":one:", ":two:", ":three:", ":four:", ":five:", ":six:",
+                                                       ":seven:", ":eight:", ":nine:", ":keycap_ten:"]
