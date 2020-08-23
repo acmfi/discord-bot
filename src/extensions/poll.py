5b827d1eeddb72a5af373d03ff549d2cb7320a3b
onmax <maximogarciamtnez@gmail.com>
2020-08-23 11:24:11 +0200

    added YesOrNoPoll with tests

--- src/extensions/poll.py
+++ src/extensions/poll.py
@@ -31,8 +31,14 @@ class PollOption:
     def set_keycap_emoji(self, index):
         if index < 1 or index > 11:
             raise Exception(f"Invalid emoji index {index}. Only valid between 0 and 9")
-            return
-        self.emoji = Emoji(EMOJIS["short"][index-1], EMOJIS["unicode"][index-1])
+        self.emoji = Emoji(EMOJIS["short"][index - 1], EMOJIS["unicode"][index - 1])
+        return self
+
+    def set_yesno_emoji(self, emoji_type):
+        if emoji_type == "tick":
+            self.emoji = Emoji(":white_check_mark:", '\U00002705')
+        elif emoji_type == "cross":
+            self.emoji = Emoji(":x:", '\U0000274c')
         return self
 
     def __str__(self):
@@ -47,14 +53,7 @@ class PollModel:
     def __init__(self, message, question):
         self.message = message
         self.question = question
-
-
-class MultipleOptionPollModel(PollModel):
-    def __init__(self, message, question, options):
-        super().__init__(message, question)
-        self.options = [PollOption(o).set_keycap_emoji(i) for i, o in enumerate(options, start=1)]
         self.poll_str = ""
-        self.set_poll_str()
 
     def set_poll_str(self):
         question = f"**{self.question}**"
@@ -62,13 +61,34 @@ class MultipleOptionPollModel(PollModel):
         self.poll_str = f"{question}\n\n{options}"
 
     def get_log(self):
-        return f"Log: Multiple option poll. Question: {self.question}. Options: {self.options}"
+        return f"Log: New poll. Question: {self.question}. Options: {[str(o) for o in self.options]}"
 
     def __str__(self):
         return self.poll_str
 
+
+class MultipleOptionPollModel(PollModel):
+    def __init__(self, message, question, options):
+        super().__init__(message, question)
+        self.options = [PollOption(o).set_keycap_emoji(i) for i, o in enumerate(options, start=1)]
+        self.set_poll_str()
+
     def __eq__(self, other):
         if isinstance(other, MultipleOptionPollModel):
+            return self.question == other.question \
+                   and False not in [o1 == o2 for (o1, o2) in zip(self.options, other.options)]
+
+
+class YesOrNoPollModel(PollModel):
+    def __init__(self, message, question):
+        super().__init__(message, question)
+        options = ["True", "False"]
+        emojis = ["tick", "cross"]
+        self.options = [PollOption(o).set_yesno_emoji(e) for o, e in zip(options, emojis)]
+        self.set_poll_str()
+
+    def __eq__(self, other):
+        if isinstance(other, YesOrNoPollModel):
             return self.question == other.question \
                    and False not in [o1 == o2 for (o1, o2) in zip(self.options, other.options)]
 
@@ -77,15 +97,14 @@ def _poll_parser(message):
     message_content = message.clean_content
     regex = r"\"(.+?)\""
     matches = list(re.finditer(regex, message_content, re.MULTILINE))
-    if (len(matches) < 3 or len(matches) > 11 or matches[-1].end() != len(message_content)) and len(matches) != 2:
+    if (len(matches) < 3 or len(matches) > 11 or matches[-1].end() != len(message_content)) and len(matches) != 1:
         raise Exception('Bro, lo has puesto mal. Si quieres un ejemplo mira cruck.\n\n\t'
                         '/poll "Aquí tu pregunta" "Una opción" "Como mínimo 2 opciones" '
                         '"Hasta 10 opciones, que guay!"')
-    if len(matches) != 2:
+    if len(matches) != 1:
         return MultipleOptionPollModel(message, matches[0].group(1), [m.group(1) for m in matches[1:]])
     else:
-        # TODO YesOrNoPoll
-        pass
+        return YesOrNoPollModel(message, matches[0].group(1))
 
 
 class Poll(commands.Cog):
@@ -98,14 +117,14 @@ class Poll(commands.Cog):
     async def create_poll(self, ctx):
         try:
             poll_model = _poll_parser(ctx.message)
-            if poll_model is None:
-                print("Not implemented yet")
-                return
             print(poll_model.get_log())
         except Exception as e:
             await ctx.message.channel.send(f"```{e}```")
             print(f"Log: Invalid input {ctx.message.clean_content} || {e}")
             return
 
+        # Send the poll
         poll_sent = await ctx.message.channel.send(poll_model.poll_str)
+
+        # React with emojis, each emoji is related to an option
         [await poll_sent.add_reaction(option.emoji.unicode) for option in poll_model.options]
--- test/test_poll.py
+++ test/test_poll.py
@@ -1,6 +1,7 @@
 import pytest
 
-from src.extensions.poll import MultipleOptionPollModel, PollModel, Emoji, EMOJIS, PollOption, _poll_parser
+from src.extensions.poll import MultipleOptionPollModel, PollModel, Emoji, EMOJIS, PollOption, \
+    YesOrNoPollModel, _poll_parser
 
 class SimulatedMessage:
     # We are using a Message model given by the Discord API. To simulate that
@@ -34,7 +35,7 @@ def test_invalid_input2():
         PollModel().from_message(SimulatedMessage(invalid_command))
 
 def test_invalid_input3():
-    invalid_command = '/poll "Just a string"'
+    invalid_command = '/poll'
     with pytest.raises(Exception):
         PollModel().from_message(SimulatedMessage(invalid_command))
 
@@ -44,11 +45,6 @@ def test_invalid_input4():
         PollModel().from_message(SimulatedMessage(invalid_command))
 
 def test_invalid_input5():
-    invalid_command = '/poll'
-    with pytest.raises(Exception):
-        PollModel().from_message(SimulatedMessage(invalid_command))
-
-def test_invalid_input6():
     invalid_command = '/poll "q" "o1" "o2" "o3" "o4" "o5" "o6" "o7" "o8" "o9" "o10" "o11"'
     with pytest.raises(Exception):
         PollModel().from_message(SimulatedMessage(invalid_command))
@@ -80,7 +76,7 @@ def test_create_option4():
     with pytest.raises(Exception):
         PollOption(option_str).set_keycap_emoji(18)
 
-def test_create_poll1():
+def test_create_multiple_poll1():
     options = ['Here an option', 'Maybe another option', 'and final option']
     poll_model = MultipleOptionPollModel(None, "Here your question", options)
     expected_str = "**Here your question**\n\n" \
@@ -91,7 +87,7 @@ def test_create_poll1():
     expected_poll.poll_str = expected_str
     assert expected_poll == poll_model
 
-def test_create_poll2():
+def test_create_multiple_poll2():
     options = ["o1", "o2", "o3", "o4", "o5", "o6", "o7", "o8", "o9", "o10"]
     poll_model = MultipleOptionPollModel(None, "q", options)
     expected_str = "**q**\n\n" \
@@ -106,5 +102,14 @@ def test_create_poll2():
                ":nine:   o9\n" \
                ":keycap_ten:   o10"
     expected_poll = MultipleOptionPollModel(None, "q", options)
+    expected_poll.poll_str = expected_str
+    assert expected_poll == poll_model
+
+def test_create_yesorno_poll1():
+    poll_model = YesOrNoPollModel(None, "Is JS the best language?")
+    expected_str = "**Is JS the best language?**\n\n" \
+                   ":white_check_mark:   True\n" \
+                   ":x:   False"
+    expected_poll = YesOrNoPollModel(None, "Is JS the best language?")
     expected_poll.poll_str = expected_str
     assert expected_poll == poll_model
