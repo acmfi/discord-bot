ddca0d05d4fcbdeb26f0e5f5a7a609ed61b294c2
Santi R <santirodrigo2001@gmail.com>
2020-09-02 18:01:09 +0200

    Modificación de estructura

--- src/funcionalidades/ban_hammer.py
+++ src/funcionalidades/ban_hammer.py
@@ -1,10 +1,11 @@
 from discord.ext import commands
+from difflib import SequenceMatcher
 
 PERMITTED_ROLES_NAMES = ('Junta', 'Admin')
 
 
 def setup(bot):
-    bot.add_cog(BanHammer(bot))
+    bot.add_cog(BanHammerView(bot))
 
 
 def have_permitted_rol(autor_roles):
@@ -14,13 +15,68 @@ def have_permitted_rol(autor_roles):
     return False
 
 
-class BanHammer(commands.Cog):
+def similar_word(a, b):
+    words = b.split(" ")
+    forbidden = [word for word in words if SequenceMatcher(None, a, word).ratio()>0.8 ]
+    #print(forbidden)
+    return forbidden
+    # More than 80% similarity
 
+
+with open('blacklist_insultos.txt', 'r') as f:
+    BLACKLIST = [line.strip().casefold()
+                    for line in f if line.strip().casefold() != ""]
+
+class BanHammer():
+    def __init__(self):
+        self.blacklist = BLACKLIST
+
+    def add_word_blacklist(self, message):
+        word = message.content.split(" ", 1)[1]
+        if word in self.blacklist:
+            return "La palabra ya estaba baneada"
+        else:
+            with open("blacklist_insultos.txt", "a") as file:
+                file.write("\n" + word)
+            self.blacklist.append(word)
+            return 'Palabra censurada correctamente :)'
+    
+    def get_forbidden_words(self, message, commands_name):
+        # Si comprobamos es comando de /censor o alias(1) y rol de autor es válido (2):
+        command_str = message.content.split()[0]
+        if have_permitted_rol(message.author.roles) and command_str in commands_name:
+            return None
+        # Obtenemos lista con palabras invalidas
+        forbidden_words_used = [i for i in self.blacklist if message.content.casefold().count(i) > 0]
+        return forbidden_words_used
+
+
+class Command:
+    def __init__(self, name, alias):
+        self.name = name
+        self.alias = alias
+
+    def get_command_n_aliases(self):
+        return [self._prefix + command for command in ([self.name] + self.alias)]
+
+
+class BanHammerCommand(Command):
+    def __init__(self):
+        super().__init__("censor", ["censura", "censurar"])
+    
+    
+
+class BanHammerView(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot
-        with open('blacklist_insultos.txt', 'r') as f:
-            self.blacklist = [line.strip().casefold()
-                              for line in f if line.strip().casefold() != ""]
+        self.bot = bot    
+        self.blacklist = BLACKLIST
+        Command._prefix = self.bot.command_prefix
+        
+
+    @commands.command(name=BanHammerCommand().name, alias=BanHammerCommand().alias)
+    @commands.has_role('Junta')
+    async def ban_word(self, ctx): 
+        await ctx.send(BanHammer().ban(ctx.message.content))
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -28,46 +84,23 @@ class BanHammer(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-
         if message.author == self.bot.user:
-            return
-
-        message_content = message.content
-        forbidden_words_used = [
-            i for i in self.blacklist if message_content.casefold().count(i) > 0]
-
-        if len(forbidden_words_used) > 0:
-            # Solo hace falta corregirlo para que no te censure a ti mismo al usar el comando censor,
-            # ya que primeron censura y luego lee el mensaje con la nueva palabra prohibida.
-            # Uncensor, descensura y luego lee la palabra que se acaba de permitir usar
-            censor_command = self.bot.get_command('censor')
-            censor_command_names = censor_command.aliases
-            censor_command_names.append(censor_command.name)
-            str_ = message_content.split()[0]
-            # If one with permitted roles is baning a word, we dont ban his message
-            if have_permitted_rol(message.author.roles) and str_[0] == self.bot.command_prefix \
-                    and str_[1:] in censor_command_names:
-                pass
-            else:
-                await message.delete()  # Delete the message
-                # Send a message on the same channel
-                await message.channel.send(message.author.mention + ", debes cuidar tu vocabulario, jovencito")
-                # Send a private message to the user
-                await message.author.send("El mensaje \n" + str(f'```diff\n-"{message_content}"```') +
+            return None
+        print(BanHammerCommand().get_command_n_aliases())
+        forbidden_words_used = BanHammer().get_forbidden_words(message=message, commands_name=BanHammerCommand().get_command_n_aliases())
+        # Si la longitud de la lista con las palabras NO validas es > 0:
+        if forbidden_words_used:
+            # Eliminar mensaje
+            await message.delete() 
+            # Avisar por el canal
+            await message.channel.send(message.author.mention + ", debes cuidar tu vocabulario, jovencito")
+            # Mensaje personal
+            # Send a private message to the user
+            await message.author.send("El mensaje \n" + str(f'```diff\n-"{message_content}"```') +
                                           "no se ajusta a las normas, en los próximos mensajes no uses: " +
                                           str(forbidden_words_used).strip('[]'))
 
-    @commands.command(name='censor')
-    @commands.has_role('Junta')
-    async def ban_word(self, ctx):
-        word = ctx.message.content.split(" ", 1)[1]
-        if word in self.blacklist:
-            await ctx.send("La palabra ya estaba baneada")
-        else:
-            with open("blacklist_insultos.txt", "a") as file:
-                file.write("\n" + word)
-            self.blacklist.append(word)
-            await ctx.send('Palabra censurada correctamente :)')
+
 
     @commands.command(name='uncensor')
     @commands.has_role('Junta')
