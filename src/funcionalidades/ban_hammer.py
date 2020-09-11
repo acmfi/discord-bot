ce7e87fdb26c1b6a661f32f2b267692d43551507
Santi R <santirodrigo2001@gmail.com>
2020-09-11 23:00:56 +0200

    Incremento cobertura test, implementación SequenceMatcher y filtrado blacklist

--- src/funcionalidades/ban_hammer.py
+++ src/funcionalidades/ban_hammer.py
@@ -15,10 +15,9 @@ def have_permitted_rol(autor_roles):
     return False
 
 
-def similar_word(a, b):
-    words = b.split(" ")
-    forbidden = [word for word in words if SequenceMatcher(None, a, word).ratio()>0.8 ]
-    #print(forbidden)
+def similar_word(message):
+    words = message.split(" ")    
+    forbidden = [insult for word in words for insult in BLACKLIST if SequenceMatcher(None, insult, word).ratio()>0.8]    
     return forbidden
     # More than 80% similarity
 
@@ -26,6 +25,7 @@ def similar_word(a, b):
 with open('src/blacklist_insultos.txt', 'r') as f:
     BLACKLIST = [line.strip().casefold()
                     for line in f if line.strip().casefold() != ""]
+                   
 
 class BanHammer():
     def __init__(self):
@@ -45,12 +45,10 @@ class BanHammer():
     def get_forbidden_words(self, message, commands_name):
         # Si comprobamos es comando de /censor o alias(1) y rol de autor es válido (2):
         command_str = message.content.split()[0]
-        #if have_permitted_rol(message.author.roles) and command_str in commands_name:
-        if have_permitted_rol(message.author.roles):
+        if have_permitted_rol(message.author.roles) and command_str in commands_name:        
             return None
         # Obtenemos lista con palabras invalidas
-        forbidden_words_used = [i for i in self.blacklist if message.content.casefold().count(i) > 0]
-        return forbidden_words_used
+        return similar_word(message.content)
 
 
     def uncensor_word(self, message):
@@ -80,7 +78,6 @@ class BanHammerCommand(Command):
         super().__init__("censor", ["censura", "censurar"])
     
     
-
 class BanHammerView(commands.Cog):
     def __init__(self, bot):
         self.bot = bot    
@@ -101,26 +98,24 @@ class BanHammerView(commands.Cog):
     async def on_message(self, message):
         if message.author == self.bot.user:
             return None
-        #print(BanHammerCommand().get_command_n_aliases())
-        forbidden_words_used = BanHammer().get_forbidden_words(message=message, commands_name=BanHammerCommand().get_command_n_aliases())
-        # Si la longitud de la lista con las palabras NO validas es > 0:
+
+        forbidden_words_used = BanHammer().get_forbidden_words(message=message, commands_name=BanHammerCommand().get_command_n_aliases())        
         if forbidden_words_used:
-            # Eliminar mensaje
+            # Delete the message
             await message.delete() 
-            # Avisar por el canal
-            await message.channel.send(message.author.mention + ", debes cuidar tu vocabulario, jovencito")
-            # Mensaje personal
+            # Send an alert through the channel
+            await message.channel.send(message.author.mention + ", debes cuidar tu vocabulario, jovencito")            
             # Send a private message to the user
             await message.author.send("El mensaje \n" + str(f'```diff\n-"{message.content}"```') +
                                           "no se ajusta a las normas, en los próximos mensajes no uses: " +
-                                          str(forbidden_words_used).strip('[]'))
-
+                                          str(forbidden_words_used).strip('[]') + "ni parecidos.")
 
 
     @commands.command(name='uncensor')
     @commands.has_role('Junta')
     async def unban_word(self, ctx):      
         await ctx.send(BanHammer().uncensor_word(ctx.message))
+
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
--- test/test_hammer.py
+++ test/test_hammer.py
@@ -7,30 +7,36 @@ class SimulatedMessage():
         self.content = msg
         self.author = SimulatedAuthor(author_name, author_role)
 
+
 class SimulatedAuthor():
     def __init__(self, name, role):
         self.name = name
         self.roles = [SimulatedRoles(role)]
 
+
 class SimulatedRoles():
     def __init__(self, role):
         self.name = role
+
 
 def load_BLACKLIST():      
     global BLACKLIST  
     with open('src/blacklist_insultos.txt', 'r') as f:
         BLACKLIST = [line.strip().casefold()
                         for line in f if line.strip().casefold() != ""]
+
                         
-load_BLACKLIST()    
+load_BLACKLIST() 
 Hammer = BanHammer()
 
 
 def test_insult():    
     assert len(Hammer.get_forbidden_words(SimulatedMessage("Eres muy tonto", "Santi", "Normal_User"), commands_name="")) == 1 
 
+
 def test_no_insult():
     assert len(Hammer.get_forbidden_words(SimulatedMessage("Hola, que tal?", "Santi", "Normal_User"), commands_name="")) == 0
+
 
 def test_several_insults():    
     assert len(Hammer.get_forbidden_words(SimulatedMessage("Eres muy tonto, imbécil y bacterio", "Santi", "Normal_User"), commands_name="")) == 3
@@ -42,9 +48,13 @@ def test_add_new_word():
     forbidden_words_used = [i for i in BLACKLIST if "PalabraABanearTest".casefold().count(i) > 0]
     assert len(forbidden_words_used) == 1
 
+
 def test_add_existing_word():  
     assert Hammer.add_word_blacklist(SimulatedMessage("!censor PalabraABanearTest", "Santi", "Junta")) == "La palabra ya estaba baneada"
+
 
 def test_remove_existing_word():  
     assert Hammer.uncensor_word(SimulatedMessage("!censor PalabraABanearTest", "Santi", "Junta")) == 'Palabra descensurada correctamente :)'
 
+def test_remove_nonexisting_word():  
+    assert Hammer.uncensor_word(SimulatedMessage("!censor PalabraABanearTest2", "Santi", "Junta")) == "La palabra no está baneada, por lo que no se ha removido"
