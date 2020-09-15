7d30ee2399b859ee0219caf32d28b074b8a38bef
Carlos González-Almansa Laredo <carlos2kgal@gmail.com>
2020-09-15 08:48:00 +0200

    Revert "Merge branch '#16' of https://github.com/acmfi/discord-bot into #16"
    This reverts commit 272ab60e7110664368fdd591d82e4e6062838b70, reversing
    changes made to df7786c1cf41ea39def443a1bdc46ca6a0580da7.

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,31 @@
+<<<<<<< Updated upstream
+import discord
+
+client = discord.Client()
+TOKEN = open('token.txt', 'r').read()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_voice_state_update(member,before,after):
+    #Si se trata de una acción dentro del mismo canal significa que no ha entrado ni salido de un canal
+    if(before.channel == after.channel):  return 0
+    for channel in member.guild.channels:
+        if str(channel).casefold() in list(map(lambda x : (str(x)+"-texto").casefold(),[after.channel,before.channel])) and channel.type.name=='text':
+            if(before.channel == None):
+                await channel.set_permissions(member,read_messages=True,send_messages= True)
+            else:
+                await channel.set_permissions(member,read_messages= False, send_messages = False)
+            break
+        
+
+
+client.run(TOKEN)
+=======
 import discord
 from discord.ext import commands
 
@@ -16,3 +44,4 @@ async def on_ready():
 
 
 bot.run(TOKEN)
+>>>>>>> Stashed changes
